fa315dde6b3d749748eed06027b7924d2231d4d2
sto.nanrong <fungi.wj@gmail.com>
2025-03-26 16:32:57 +0800

    addd some data

--- test.py
+++ test.py
@@ -1,7 +1,19 @@
 from fireredasr.models.fireredasr import FireRedAsr
 
 batch_uttid = ["BAC009S0764W0121"]
-batch_wav_path = ["examples/wav/BAC009S0764W0121.wav"]
+batch_wav_path = ["examples/wav/multidomain_sample1.wav",
+                  "examples/wav/multidomain_sample2.wav",
+                  "examples/wav/multidomain_sample3.wav",
+                  "examples/wav/multidomain_sample4.wav",
+                  "examples/wav/multidomain_sample5.wav",
+                  "examples/wav/multidomain_sample6.wav",
+                  "examples/wav/multidomain_sample7.wav",
+                  "examples/wav/multidomain_sample8.wav",
+                  "examples/wav/sichuan_dialect_sample.wav",
+                  "examples/wav/wuu_dialect_sample.wav",
+                  "examples/wav/xiang_dialect_sample.wav",
+                  "examples/wav/zgyu_dialect_sample.wav",
+                  "examples/wav/cantonese_dialect_sample.wav"]
 
 # FireRedASR-AED
 model = FireRedAsr.from_pretrained("aed", "pretrained_models/FireRedASR-AED-L")
